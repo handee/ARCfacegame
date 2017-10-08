80ce5c7547bb6ce9a45c7e351e3331270ec5ded8
Hannah <hmd1@aber.ac.uk>
2017-10-08 21:09:16 +0100

    built up moving drops

---
+++ drops5.py
@@ -0,0 +1,55 @@
+
+import cv2
+import video
+import sys
+import numpy as np
+from random import randint
+
+
+# These are variables, which decide how big the picture is going to be
+# and how many milliseconds to wait before updating the picture
+width=400
+height=400
+frame_length=10
+
+drop_size=10
+x_position=200
+y_position=5
+blue=255
+green=100
+red=100
+
+# Open a window
+cv2.namedWindow('drops')
+
+# Create an image 
+myimage=np.zeros((height,width,3), np.uint8)
+
+
+while True:
+   myimage=np.zeros((height,width,3), np.uint8)
+
+   # Change the y_position of the drop
+   y_position=y_position+1
+   
+   # If it goes off the bottom of the screen, put it back at the top
+   if (y_position>height):
+      y_position=5
+      # move the x_position to a random place from 0 (left-hand-side) 
+      # to width (right-hand-side)
+      x_position=randint(0,width)
+
+   # Draw a circle on the image    
+   cv2.circle(myimage,(x_position,y_position), drop_size, (blue,green,red), -1)
+
+
+   # Show the image in the window
+   cv2.imshow('drops',myimage)
+   
+   # Wait a bit for a key to be pressed. This line also redraws the 
+   # pictures so it's important
+   ch=cv2.waitKey(frame_length) & 0xFF
+
+   # If that key is 27 ("ESC") then quit
+   if ch==27:
+      break
---
+++ facedetect.py
@@ -0,0 +1,45 @@
+
+import cv2
+import video
+import common
+import sys
+import numpy as np
+from random import randint
+
+b=50
+g=50
+r=200
+colour=(b,g,r)
+
+#The face detector can have minimum and maximum sizes. 
+mn=30 # minimum face width
+mx=500 # maximum face width
+
+#these two lines set up the face detector and the video input
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cam = video.create_capture(0)
+
+while True:
+   # read an image from the video camera
+   ret, img = cam.read()
+   
+   # convert the image to greyscale (black and white) storing it in the 
+   # variable "grey"
+   grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   
+   # run the face detector and store the output in the variable "rects"
+   rects = cascade.detectMultiScale(grey, scaleFactor=1.3, minNeighbors=3, minSize=(mn, mn), maxSize=(mx,mx))
+
+   # loop through and draw the detected faces on the image
+   for x, y, w, h in rects:
+      cv2.rectangle(img, (x, y), (x+w, y+h), colour, 2)
+
+   cv2.imshow('facedetect', img)
+
+   if 0xFF & cv2.waitKey(5) == 27:
+      break
+
+
+cv2.destroyAllWindows()
+
+
